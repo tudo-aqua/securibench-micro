ca390c692b3fe04f72f3053ffa77d001343b5c70
feho243 <felix.holewa@tu-dortmund.de>
2022-05-24 20:10:06 +0200

    Removed another boolean parameter

--- generate-svcomp-benchmarks.sh
+++ generate-svcomp-benchmarks.sh
@@ -58,8 +58,8 @@ public class Main {
   public static void main(String[] args) {
     String s1 = Verifier.nondetString();
 
-    Tainting.taint(s1, sql_injection, false);
-    Tainting.taint(s1, xss, false);
+    Tainting.taint(s1, sql_injection);
+    Tainting.taint(s1, xss);
 
     HttpServletRequest req = new HttpServletRequest();
     HttpServletResponse res = new HttpServletResponse();
