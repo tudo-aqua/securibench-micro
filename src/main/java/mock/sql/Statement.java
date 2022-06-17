ef2579ff8558bcc588cf428859fa548661a72ac9
Malte Mues <mail.mues@gmail.com>
2022-06-17 14:33:46 +0200

    Fix signature to avoid compile errors

--- src/main/java/mock/sql/Statement.java
+++ src/main/java/mock/sql/Statement.java
@@ -12,11 +12,11 @@ import org.sosy_lab.sv_benchmarks.Tainting;
 // Uses Tainting class to check for SQL-Injection
 public class Statement {
 
-    public void execute(String s) {
+    public void execute(String s) throws SQLException{
         Tainting.check(s, Tainting.SQL_INJECTION);
     }
 
-    public void executeUpdate(String s, Object ... o) {
+    public void executeUpdate(String s, Object ... o) throws SQLException {
         Tainting.check(s, Tainting.SQL_INJECTION);
     }
 
