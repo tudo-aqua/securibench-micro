3b75c5fed98a053fb6520e93a0909af26244b4a4
Richard Stewing <richy.sting@gmail.com>
2022-11-30 05:13:02 +0100

    implements a read'' as cast from character

--- src/main/java/mockx/servlet/ServletInputStream.java
+++ src/main/java/mockx/servlet/ServletInputStream.java
@@ -3,26 +3,26 @@ package mockx.servlet;
 import java.io.InputStream;
 
 public class ServletInputStream extends InputStream {
-    private byte[] string;
+    private String string;
     private int position;
     private int mark;
     
     public ServletInputStream(String string) {
 	if(string == null) {
-	    this.string = new byte[0];
+	    this.string = "";
 	} else {
-	    this.string = string.getBytes();
+	    this.string = string;
 	}
 	this.position = 0;
 	this.mark = 0;
     }
 
     public int available() {
-	return string.length - position;
+	return string.length() - position;
     }
 
     public void close(){
-	position = string.length;
+	position = string.length();
     }
 
     public void mark () {
@@ -30,10 +30,12 @@ public class ServletInputStream extends InputStream {
     }
 
     public int read() {
-	if(position >= string.length) {
+	if(position >= string.length()) {
 	    return -1;
 	} else {
-	    byte nextByte = string[position];
+	    // Copies the lower 8-bits of the character into the next byte.
+	    // As of 30.11.2022 this is consistent with the behavior of .getBytes().
+	    byte nextByte = (byte) string.charAt(position);
 	    position++;
 	    return nextByte;
 	}
@@ -44,7 +46,7 @@ public class ServletInputStream extends InputStream {
     }
 
     public long skip(long step){
-	int maxstep = string.length - position;
+	int maxstep = string.length() - position;
 	if(step > maxstep) {
 	    position += maxstep;
 	    return maxstep;
